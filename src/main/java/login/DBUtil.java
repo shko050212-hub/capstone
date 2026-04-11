373de693b5486755e29f5c99b71fa0cb566971fb
shko050212-hub <shko050212@gmail.com>
2026-04-11 21:24:28 +0900

    Update DBUtil.java

--- src/main/java/login/DBUtil.java
+++ src/main/java/login/DBUtil.java
@@ -6,7 +6,9 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/login_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String URL =
+        "jdbc:mysql://218.156.143.205:3306/login_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
+
     private static final String USER = "root";
     private static final String PASSWORD = "1234";
 
@@ -14,7 +16,7 @@ public class DBUtil {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL JDBC Driver 로드 실패", e);
+            throw new SQLException("드라이버 로드 실패", e);
         }
 
         return DriverManager.getConnection(URL, USER, PASSWORD);
