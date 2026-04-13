7a068762e2d2bf1b9d9dc97e53360d9ba0c445f8
shko050212-hub <shko050212@gmail.com>
2026-04-14 01:20:24 +0900

    Update DBUtil.java

--- GS_capstone-main/project1/src/main/java/login/DBUtil.java
+++ GS_capstone-main/project1/src/main/java/login/DBUtil.java
@@ -8,14 +8,15 @@ public class DBUtil {
 
     private static final String URL =
         "jdbc:mysql://capstone-db.cfw4cygk4dks.ap-northeast-2.rds.amazonaws.com:3306/login_db?serverTimezone=Asia/Seoul&useSSL=false&allowPublicKeyRetrieval=true";
-    private static final String USER = "실제DB아이디";
-    private static final String PASSWORD = "실제DB비밀번호";
+
+    private static final String USER = "여기에_DB아이디";
+    private static final String PASSWORD = "여기에_DB비밀번호";
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("MySQL JDBC Driver 로드 실패", e);
+            throw new RuntimeException("MySQL 드라이버 로드 실패", e);
         }
     }
 
