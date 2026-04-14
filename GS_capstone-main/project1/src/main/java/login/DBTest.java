69bd7f94d36037241b21bfcfb3f207c35f971c5e
shko050212-hub <shko050212@gmail.com>
2026-04-14 10:53:01 +0900

    Update DBTest.java

--- GS_capstone-main/project1/src/main/java/login/DBTest.java
+++ GS_capstone-main/project1/src/main/java/login/DBTest.java
@@ -1,22 +1,25 @@
-package login;
-
-import java.sql.Connection;
-
-public class DBTest {
-    public static void main(String[] args) {
-        try {
-            Connection conn = DBUtil.getConnection();
-
-            if (conn != null) {
-                System.out.println("DB 연결 성공!");
-                conn.close();
-            } else {
-                System.out.println("DB 연결 실패");
-            }
-
-        } catch (Exception e) {
-            System.out.println("에러 발생!");
-            e.printStackTrace();
-        }
-    }
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.sql.Connection" %>
+<%@ page import="login.DBUtil" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>DB 연결 테스트</title>
+</head>
+<body>
+<h2>DB 연결 테스트</h2>
+<%
+try {
+    Connection conn = DBUtil.getConnection();
+    out.println("<p style='color:green;'>DB 연결 성공</p>");
+    conn.close();
+} catch (Exception e) {
+    out.println("<p style='color:red;'>DB 연결 실패</p>");
+    out.println("<pre>");
+    e.printStackTrace(new java.io.PrintWriter(out));
+    out.println("</pre>");
 }
+%>
+</body>
+</html>
